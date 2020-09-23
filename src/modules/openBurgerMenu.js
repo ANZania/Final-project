20c34d26857e63bafaf95f2d7cf467d700feaf70
ANZania <zianastasia@yandex.ru>
2020-09-23 23:29:03 +0300

    Add menu roll-up effect on phones

--- src/modules/openBurgerMenu.js
+++ src/modules/openBurgerMenu.js
@@ -3,18 +3,35 @@ const openBurgerMenu = () => {
         popupMenu = document.querySelector('.popup-menu'),
         popupDialogMenu = popupMenu.querySelector('.popup-dialog-menu');
 
-    menuIcon.addEventListener('click', () => {
+    const width = document.documentElement.clientWidth;
+
+    const openDialogMenu = () => {
         popupMenu.style.visibility = 'visible';
-        popupDialogMenu.style.right = '639px';
-    });
+
+        if (width <= 576) {
+            popupDialogMenu.style.top = '545px';
+        } else {
+            popupDialogMenu.style.right = '639px';
+        }
+    };
+
+    const closeDialogMenu = () => {
+        popupMenu.style.visibility = 'hidden';
+
+        if (width <= 576) {
+            popupDialogMenu.style.top = '0px';
+        } else {
+            popupDialogMenu.style.right = '0px';
+        }
+
+    }
+
+    menuIcon.addEventListener('click', openDialogMenu);
 
     popupDialogMenu.addEventListener('click', event => {
         const target = event.target;
 
-        if (target.matches('.close-menu')) {
-            popupMenu.style.visibility = 'hidden';
-            popupDialogMenu.style.right = '0px';
-        }
+        if (target.matches('.close-menu')) closeDialogMenu();
     });
 };
 
