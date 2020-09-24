3d1d4c3bd57e4b497908c18c2202b29a3e548f76
ANZania <zianastasia@yandex.ru>
2020-09-24 22:01:49 +0300

    Add privacy popup

--- src/index.js
+++ src/index.js
@@ -5,9 +5,11 @@ import openBurgerMenu from './modules/openBurgerMenu';
 import footerScroll from './modules/footerScroll';
 import addFullPriceList from './modules/fullPriceList';
 import phoneMask from './modules/phoneMask';
+import togglePrivacy from './modules/togglePrivacy';
 
 addPhoneProlapse();
 openBurgerMenu();
 footerScroll();
 addFullPriceList();
 phoneMask();
+togglePrivacy();
